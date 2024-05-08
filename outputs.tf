cf4ceda4bcafbbd2fadcc9aabcc8fabc777cab8b
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-05-08 13:35:29 -0400

    Alphabetize

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "rule" {
-  value       = aws_cloudwatch_event_rule.this
   description = "The EventBridge event rule that will be triggered when a new IAM or SSO user is created."
+  value       = aws_cloudwatch_event_rule.this
 }
 
 output "target" {
-  value       = aws_cloudwatch_event_target.this
   description = "The EventBridge event target for the rule."
+  value       = aws_cloudwatch_event_target.this
 }
